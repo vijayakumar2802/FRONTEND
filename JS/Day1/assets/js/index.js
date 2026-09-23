2f35b863f5d2b5c57c96cb4cdebfa8cd75dc2bc4
vijay <vijay28282005@gmail.com>
2026-09-23 18:25:36 +0530

    updated

---
+++ JS/Day1/assets/js/index.js
@@ -0,0 +1,9 @@
+
+var name = "vijay";
+let age = 21;
+let course = "Fullstack Development";
+console.log(name);
+console.log(age);
+console.log(course);
+    
+
---
+++ JS/Day2/assets/js/index.js
@@ -0,0 +1,71 @@
+let appleIphone = "iPhone";
+const samsungGalaxy = "Galaxy";
+var googlePixel = "Pixel";
+let oneplus = "Nord";
+const redmiNote = "Redmi";
+var realmeNarzo = "Narzo";
+let vivoVSeries = "V Series";
+const oppoReno = "Reno";
+var nothingPhone = "Nothing";
+let motorolaEdge = "Edge";
+
+let vanillaScoop = 10;
+const chocolateCone = 20;
+var strawberryCup = 30;
+let mangoScoop = 40;
+const caramelCone = 50;
+var pistachioCup = 60;
+let blueberry = 70;
+const butterscotch = 80;
+var blackcurrant = 90;
+let cookiesCream = 100;
+
+let cupcake = true;
+const cheeseCake = false;
+var plumcake = true;
+let redvelvet = false;
+const honeycake = true;
+
+let oreaoCream = null;
+var darkFantasy = null;
+const bourbon = null;
+
+let dairyMilk;
+var kitKat;
+let fiveStar;
+            
+console.log(appleIphone);
+console.log(samsungGalaxy);
+console.log(googlePixel);
+console.log(oneplus);
+console.log(realmeNarzo);
+console.log(vivoVSeries);
+console.log(redmiNote);
+console.log(oppoReno);
+console.log(nothingPhone);
+console.log(motorolaEdge);
+
+console.log(vanillaScoop);
+console.log(chocolateCone);
+console.log(strawberryCup);
+console.log(mangoScoop);
+console.log(caramelCone);
+console.log(pistachioCup);
+console.log(blueberry);
+console.log(butterscotch);
+console.log(blackcurrant);
+console.log(cookiesCream);
+
+console.log(cupcake);
+console.log(cheeseCake);
+console.log(plumcake);
+console.log(redvelvet);
+console.log(honeycake);
+
+console.log(oreaoCream);
+console.log(darkFantasy);
+console.log(bourbon);
+
+console.log(dairyMilk);
+console.log(kitKat);
+console.log(fiveStar);
